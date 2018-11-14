f8305fcfae1fd5df9c7a5879a16292be45ff2ff1
grandeto <grandeto.com@gmail.com>
2018-11-14 02:24:08 +0200

    Front-end config

---
+++ resources/views/consumers/index.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="//cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <title>Consumer Manager</title>
+</head>
+<body>
+    <h1>Consumer Manager</h1>
+    <div id="app"></div>
+<script src="{{ mix('js/app.js') }}"></script>
+<script>
+    window.axios.defaults.headers.common = {
+        'X-Requested-With': 'XMLHttpRequest',
+        'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+    };
+</script>
+</body>
+</html>
