c656f57518bb51e85f7f76b2f2f16869e30039da
grandeto <grandeto.com@gmail.com>
2018-11-14 02:31:25 +0200

    Remove unnecessary stylesheet

--- resources/views/consumers/index.blade.php
+++ resources/views/consumers/index.blade.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="//cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>Consumer Manager</title>
