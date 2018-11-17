ea57dabf42be210ba2bfae12ea2b88491cb912ed
grandeto <grandeto.com@gmail.com>
2018-11-17 18:41:57 +0200

    Authentication: add and change blade templates

--- resources/views/consumers/index.blade.php
+++ resources/views/consumers/index.blade.php
@@ -1,23 +1,5 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-    <title>Consumer Manager</title>
-</head>
-<body>
-    <h1>Consumer Manager</h1>
-    <div id="app"></div>
-<script src="{{ mix('js/app.js') }}"></script>
-<script>
-    window.axios.defaults.headers.common = {
-        'X-Requested-With': 'XMLHttpRequest',
-        'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
-    };
-</script>
-</body>
-</html>
+@extends('layouts.app')
+@section('content')
+<h1>{{ config('app.name', 'Laravel') }}</h1>
+<div id="consumers"></div>
+@endsection
---
+++ resources/views/nav/about.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<div class="about">Hello, this is the about page!</div>
+@endsection
